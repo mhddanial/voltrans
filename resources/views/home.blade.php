f1085470ad88c240eabed3a07cbbdfdc65acb269
Muhammad Danial <mhd2danial3@gmail.com>
2025-03-15 17:20:22 +0700

    Instalasi Laravel 12 dan library Daisy Ui untuk tailwind css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>HomePage</title>
+</head>
+<body>
+    <h2 class="text-success font-5xl">Tes 123 ini Home</h2>
+</body>
+</html>
